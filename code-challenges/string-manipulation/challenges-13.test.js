29851ed3efdca9efe4076b9ac256c0395d188fc1
Karina Chen <karina6188@gmail.com>
2019-09-26 10:12:44 -0700

    completed code challenge-13

--- code-challenges/string-manipulation/challenges-13.test.js
+++ code-challenges/string-manipulation/challenges-13.test.js
@@ -60,7 +60,13 @@ For example, 'abcdefg' returns 'bdf'
 ------------------------------------------------------------------------------------------------ */
 
 const onlyOddChars = (str) => {
-  // Solution code here...
+  let odd = [];
+  let even = [];
+  let separate = str.split('');
+  for (let i = 0; i < separate.length; i++){
+    i % 2 === 0 ? even.push(separate[i]) : odd.push(separate[i]);
+  }
+  return odd.join('');
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -104,7 +110,7 @@ const findEvery = (arr, target) => {
   arr.forEach(string => {
     result.push(string.includes(target));
   })
-    return result.includes(false) ? false : true;
+  return result.includes(false) ? false : true;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -120,7 +126,14 @@ For example, [['Brook Testing', 'Actual Person'], ['Human Person', 'Brook again'
 ------------------------------------------------------------------------------------------------ */
 
 const unenrollBrook = (arr) => {
-  // Solution code here...
+  let final = [];
+  arr.forEach(array => {
+    let result = array.filter(value => {
+      return !value.includes('Brook');
+    });
+    final.push(result);
+  })
+  return final;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -203,7 +216,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   test('It should only return the odd indexed characters from the string', () => {
     expect(onlyOddChars('0123456789')).toStrictEqual('13579');
     expect(onlyOddChars('abcd')).toStrictEqual('bd');
@@ -241,7 +254,7 @@ describe('Testing challenge 7', () => {
   });
 });
 
-xdescribe('Testing challenge 8', () => {
+describe('Testing challenge 8', () => {
   test('It should remove Brook from all courses', () => {
     const roster = [
       ['Michelle', 'Allie', 'Brook TESTING'],
